c7f71dd7705026341747a1437895c6369f90c71f
prasad522 <prasad522.ambati@gmail.com>
2024-06-24 20:11:19 +0530

    modified main.tf file

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "key_pair" {
-  source     = "../../modules/key-pair"
-  public_key = file("../../.ssh/id_ed25519.pub")
+  source     = "./modules/key-pair"
+  public_key = file(".ssh/id_ed25519.pub")
   org        = var.org
   env        = var.env
 }
 
 module "ec2-instance" {
-  source                    = "../../modules/ec2-instance"
+  source                    = "./modules/ec2-instance"
   ec2_ami_id                = var.ec2_ami_id
   instance_type             = var.instance_type
   key_pair_name             = module.key_pair.key_pair_name
@@ -15,30 +15,30 @@ module "ec2-instance" {
   security_group_id = module.security_group.security_group_id
   org                       = var.org
   env                       = var.env
-  user_data_jenkins_install = file("../../Jenkins-Install/jenkins_-install.sh")
+  user_data_jenkins_install = file("Jenkins-Install/jenkins_-install.sh")
 }
 
 module "vpc" {
-  source     = "../../modules/vpc"
+  source     = "./modules/vpc"
   cidr_block = var.cidr_block
   org        = var.org
   env        = var.env
 }
 
 module "security_group" {
-  source = "../../modules/security-group"
+  source = "./modules/security-group"
   vpc_id = module.vpc.vpc_id
   org    = var.org
   env    = var.env
 }
 
 module "internet_gw"{
-  source = "../../modules/internet_gate_way"
+  source = "./modules/internet_gate_way"
   vpc_id = module.vpc.vpc_id
 }
 
 module "public_subnets" {
-  source             = "../../modules/subnet"
+  source             = "./modules/subnet"
   vpc_id             = module.vpc.vpc_id
   subnet_cidr_blocks = var.public_subnet_cidr_blocks
   availability_zones = var.availability_zones
@@ -48,7 +48,7 @@ module "public_subnets" {
 }
 
 module "private_subnets" {
-  source             = "../../modules/subnet"
+  source             = "./modules/subnet"
   vpc_id             = module.vpc.vpc_id
   subnet_cidr_blocks = var.private_subnet_cidr_blocks
   availability_zones = var.availability_zones
@@ -58,7 +58,7 @@ module "private_subnets" {
 }
 
 module "public_route_table" {
-  source = "../../modules/route-table"
+  source = "./modules/route-table"
   vpc_id = module.vpc.vpc_id
   igw_id = module.internet_gw.igw_id
   org = var.org
