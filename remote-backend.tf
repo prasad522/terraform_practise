12071f1c79d7e31bc2ef37ace1d3875723ea1f6a
prasad522 <prasad522.ambati@gmail.com>
2024-06-24 14:58:51 +0530

    changed region eu-west-1

--- remote-backend.tf
+++ remote-backend.tf
@@ -1,6 +1,7 @@
-
 terraform {
   backend "s3" {
-
+    bucket = "prasad522-default-terraform-remote-state"  // Replace with your actual S3 bucket name
+    key    = "prasad522/default/terraform.tfstate"  // Optional: Specify the path to the state file within the bucket
+    region = "eu-west-1"  // Replace with your AWS region
   }
 }
